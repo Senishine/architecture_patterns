8d31e318e73452441161a371eccabd8ba18c9528
Senishine <oksastar89@mail.ru>
2022-04-01 23:38:22 +0300

    lesson3 add files

--- views.py
+++ views.py
@@ -3,16 +3,16 @@ from my_framework.rendering import render
 
 class HomePage:
     def __call__(self, request):
-        return '200 OK', render('index.html', title="SpeakEnglish")
+        return '200 OK', render('index.html')
 
 
 class About:
     def __call__(self, request):
-        return '200 OK', render('about.html', title="About us")
+        return '200 OK', render('about.html')
 
 
 class Contact:
     def __call__(self, request):
-        return '200 OK', render('contact.html', title="Contacts")
+        return '200 OK', render('contact.html')
 
 
